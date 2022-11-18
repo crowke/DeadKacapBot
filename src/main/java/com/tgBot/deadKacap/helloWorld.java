d2ea1d6f27415d60818671bb7b5b0bd14702e9dd
qlspd <ytrewq0loh@gmail.com>
2022-11-18 15:45:44 +0200

    реорганізація коду заради його розуміння

--- src/main/java/com/tgBot/deadKacap/helloWorld.java
+++ src/main/java/com/tgBot/deadKacap/helloWorld.java
@@ -17,12 +17,102 @@ import java.util.regex.Pattern;
 
 @Component
 public class helloWorld extends TelegramLongPollingBot {
+    static boolean kacap = false;
+    static boolean send = false;
 
-    public boolean setText(String text, SendMessage sm, Message message) {
+    @Override
+    public void onUpdateReceived(Update update) {
+        Message message = update.hasMessage() ? update.getMessage()
+                : update.hasEditedMessage() ? update.getEditedMessage()
+                : null;
+        if (message != null && (message.getChat().isGroupChat() || message.getChat().isSuperGroupChat())) {
+            String text = message.getText().toLowerCase();
+            Scanner messc = new Scanner(text);
+            SendMessage sm = new SendMessage(); DeleteMessage dm = new DeleteMessage();
+            kacapWords1(text, sm, message);
+            kacapWords2(text, sm, message);
+            rusEng(messc);
+            checkWords(text, sm, message);
+            try {
+                if (kacap) {
+                    dm.setChatId(message.getChatId());
+                    dm.setMessageId(message.getMessageId());
+                    execute(dm);
+                }
+                if (send) { execute(sm); }
+            } catch (TelegramApiException e) {
+                displayLog(message, e);
+            }
+        }
+    }
+
+    public static boolean setText(String text, SendMessage sm, Message message) {
         sm.setText(text);
         sm.setChatId(message.getChatId());
         return true;
     }
+    public static void kacapWords1(String text, SendMessage sm, Message message) {
+        for (String kacapWord : kacapWords) {
+            if (text.contains(kacapWord)) {
+                kacap = true;
+                send = setText("повідомлення видалено через " + (kacapWord.length() <= 2 ? "букву" : "слово")
+                        + " \"" + kacapWord + "\"", sm, message);
+                break;
+            }
+        }
+    }
+    public static void kacapWords2(String text, SendMessage sm, Message message) {
+        for (String kacapWord2 : kacapWords2) {
+            Pattern pattern = Pattern.compile("\\b" + kacapWord2 + "\\b");
+            Matcher matcher = pattern.matcher(text);
+            kacap = matcher.find();
+            if (kacap) {
+                send = setText("повідомлення видалено через слово \"" + kacapWord2 + "\"", sm, message);
+                break;
+            }
+        }
+    }
+    public static void rusEng(Scanner messc) {
+        while (messc.hasNext()) {
+            String word = messc.next();
+            for (char eng = 97; eng <= 122; eng++) {
+                for (char rus = 1072; rus <= 1103; rus++) {
+                    if (word.contains("" + eng) && word.contains("" + rus)) { kacap = true; break; }
+                }
+            }
+        }
+    }
+    public static void checkWords(String text, SendMessage sm, Message message) {
+        String[] inputs = {"/start", "/start@deadkacapbot", "слава Україні".toLowerCase(), "слава нації",
+                "путін", "путин", "путлер", "путлєр"};
+        String[] outputs = {"привіт! я запущений і прямо зараз працюю!", "героям слава!", "смерть кацапам!",
+                "путін - хуйло! кацапи - нелюди!"};
+        int j = 0;
+        for (int i = 0; i < inputs.length; i++) {
+            j += (i < 2 || i > 4 ? 0 : 1);
+            send = text.contains(inputs[i]) ? setText(outputs[j], sm, message) : send;
+        }
+    }
+    public static void displayLog(Message message, Exception e) {
+        long chatID = Math.abs(message.getChatId());
+        System.out.println("[" + LocalTime.now().format(DateTimeFormatter.ofPattern("HH:mm:ss")) + "] "
+                + (e.getMessage().contains("message can't be deleted")
+                ? "повідомлення не може бути видалене" : e.getMessage())
+                + ": " + message.getChat().getTitle() + "; https://t.me/c/"
+                + (chatID > 10_000_000_000L ? chatID - 1_000_000_000_000L : chatID)
+                + "/" + message.getMessageId());
+    }
+    static String[] kacapWords = {"э", "ы", "ъ", "ё", "ьі", "привет", "дела", "что", "заебись", "тупой", "спасибо",
+            "слушай", "тебя", "работ", "свободн", "ебат", "здарова", "почему", "ебал", "когда", "только", "почт", "пример",
+            "русс", "росси", "пидорас", "пидарас", "нихуя", "хуел", "пиздо", "понял", "еблан", "далее", "запрет",
+            "меня", "добавь", "другой", "совсем", "понятно", "брос", "освобо", "согл", "хотел", "наверно", "хуеть", "игра",
+            "мальчик", "девочк", "здрасте", "здравствуй", "надеюс", "вреш", "скольк", "поздр", "разговари", "нрав", "слуша",
+            "двое", "трое", "сейчас"};
+    static String[] kacapWords2 = {"как", "кто", "никто", "некто", "он", "его", "она", "оно", "они", "их", "еще", "што",
+            "пон", "нипон", "непон", "кринж", "какой", "какие", "каких", "нет", "однако", "пока", "если", "меня",
+            "сегодня", "и", "иди", "потом", "дашь", "пиздец", "лет", "мне", "ищу", "надо", "мой", "твой", "свои", "свой",
+            "зачем", "нужно", "надо", "всем", "есть", "ебет", "ща", "щя", "щас", "щяс", "либо", "может", "любой", "любая",
+            "че", "чего"};
     @Value("${telegram.bot.username}")
     private String username;
     @Value("${telegram.bot.token}")
@@ -35,82 +125,5 @@ public class helloWorld extends TelegramLongPollingBot {
     @Override
     public String getBotToken() {
         return token;
-    }
-
-    @Override
-    public void onUpdateReceived(Update update) {
-        String[] kacapWords = {"э", "ы", "ъ", "ё", "ьі", "привет", "дела", "что", "заебись", "тупой", "спасибо",
-        "слушай", "тебя", "работ", "свободн", "ебат", "здарова", "почему", "ебал", "когда", "только", "почт", "пример",
-        "русс", "росси", "пидорас", "пидарас", "нихуя", "хуел", "пиздо", "понял", "еблан", "далее", "запрет",
-        "меня", "добавь", "другой", "совсем", "понятно", "брос", "освобо", "согл", "хотел", "наверно", "хуеть", "игра",
-        "мальчик", "девочк", "здрасте", "здравствуй", "надеюс", "вреш", "скольк", "поздр", "разговари", "нрав", "слуша",
-        "двое", "трое", "сейчас"};
-        String[] kacapWords2 = {"как", "кто", "никто", "некто", "он", "его", "она", "оно", "они", "их", "еще", "што",
-        "пон", "нипон", "непон", "кринж", "какой", "какие", "каких", "нет", "однако", "пока", "если", "меня",
-                "сегодня", "и", "иди", "потом", "дашь", "пиздец", "лет", "мне", "ищу", "надо", "мой", "твой", "свои", "свой",
-        "зачем", "нужно", "надо", "всем", "есть", "ебет", "ща", "щя", "щас", "щяс", "либо", "может", "любой", "любая"};
-        Message message = update.hasMessage() ? update.getMessage()
-                : update.hasEditedMessage() ? update.getEditedMessage()
-                : null;
-        if (message != null && (message.getChat().isGroupChat() || message.getChat().isSuperGroupChat())) {
-            String text = message.getText().toLowerCase();
-            Scanner messc = new Scanner(text);
-            SendMessage sm = new SendMessage(); DeleteMessage dm = new DeleteMessage();
-            boolean kacap = false; boolean send = false;
-            if (text.equals("/start") || text.equals("/start@deadkacapbot")) {
-                send = setText("привіт! я запущений і прямо зараз працюю!", sm, message);
-            } else {
-                for (String kacapWord : kacapWords) {
-                    if (text.contains(kacapWord)) {
-                        kacap = true;
-                        send = setText("повідомлення видалено через " + (kacapWord.length() <= 2 ? "букву" : "слово")
-                                + " \"" + kacapWord + "\"", sm, message);
-                        break;
-                    }
-                }
-                if (!kacap) {
-                    for (String kacapWord2 : kacapWords2) {
-                        Pattern pattern = Pattern.compile("\\b" + kacapWord2 + "\\b");
-                        Matcher matcher = pattern.matcher(text);
-                            kacap = matcher.find();
-                            if (kacap) {
-                                send = setText("повідомлення видалено через слово \"" + kacapWord2 + "\"", sm, message);
-                                break;
-                            }
-                    }
-                }
-                if (!kacap) {
-                    loop: while (messc.hasNext()) {
-                        String word = messc.next();
-                        for (char eng = 97; eng <= 122; eng++) {
-                            for (char rus = 1072; rus <= 1103; rus++) {
-                                if (word.contains("" + eng) && word.contains("" + rus)) { kacap = true; break loop; }
-                            }
-                        }
-                    }
-                }
-                String[] putlerWords = {"путін", "путин", "путлер", "путлєр"};
-                for (String putlerWord : putlerWords) {
-                    send = send || text.contains(putlerWord) && setText("путін - хуйло! кацапи - нелюди!", sm, message);
-                    if (send) { break; }
-                }
-                send = send || text.contains("слава Україні".toLowerCase()) && setText("героям слава!", sm, message)
-                        || text.contains("слава нації") && setText("смерть кацапам!", sm, message);
-            }
-            dm.setChatId(message.getChatId());
-            dm.setMessageId(message.getMessageId());
-            try {
-                if (kacap) { execute(dm); }
-                if (send) { execute(sm); }
-            } catch (TelegramApiException e) {
-                long chatID = Math.abs(message.getChatId());
-                System.out.println("[" + LocalTime.now().format(DateTimeFormatter.ofPattern("HH:mm:ss")) + "] "
-                + (e.getMessage().contains("message can't be deleted")
-                ? "повідомлення не може бути видалене" : e.getMessage())
-                + ": " + message.getChat().getTitle() + "; https://t.me/c/"
-                + (chatID > 10_000_000_000L ? chatID - 1_000_000_000_000L : chatID)
-                + "/" + message.getMessageId());
-            }
-        }
     }
 }
